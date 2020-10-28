6075fccae32db22308d3e8eb310845b75700601d
Luiz Gabriel Agana <28706022+zarielan@users.noreply.github.com>
2020-10-28 17:28:33 +0800

    Copy pasted main program from animation1.dart to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,53 @@ import 'package:flutter/material.dart';
 
 import 'routes.dart';
 
-//void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  // The parent widget.
+
+  Tween<double> _scaleTween = Tween<double>(begin: 1, end: 2);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // removes the debug banner
+      title: 'Flutter To-do List',
+
+      home: Scaffold(
+        body: Center(
+          child: TweenAnimationBuilder(
+            tween: _scaleTween,
+            duration: Duration(milliseconds: 900),
+            builder: (context, scale, child){
+              return Transform.scale(scale: scale, child: child);
+            },
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.lightBlue[200],
+              child: Center(
+                child: Text(
+                  'TEST XD',
+                  style: Theme.of(context).textTheme.headline5,
+                ),  //Text
+              ),  //Center
+            ),  //Container
+          ),
+        ),  //Center
+      ),  //Scaffold
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Baloo_Chettan_2",
+      ),
+      initialRoute: '/',
+      routes: mainAppRoutes, // See 'routes.dart' for this
+    );
+  }
+}
+
+/*
 class MyApp extends StatelessWidget {
   // The parent widget.
   @override
@@ -22,3 +67,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
+*/
